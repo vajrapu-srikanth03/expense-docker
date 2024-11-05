08393696694a98bcefbdac9b30222cf710a01fe7
srikanthg <skanth306@gmail.com>
2024-11-05 17:02:36 +0530

    expense-docker

--- expense-docker/backend/mysql-check.sh
+++ expense-docker/backend/mysql-check.sh
@@ -1,17 +1,26 @@
-MYSQL_CONTAINER="mysql"
-MYSQL_USER="root"
-MYSQL_PASS="ExpenseApp@1"
+# MYSQL_CONTAINER="mysql"
+# MYSQL_USER="root"
+# MYSQL_PASS="ExpenseApp@1"
 
-check_mysql() {
-    docekr exec "$MYSQL_CONTAINER" mysqladmin -u "$MYSQL_USERt" -p"$MYSQL_PASS" ping --silent
-}
+# check_mysql() {
+#     docekr exec "$MYSQL_CONTAINER" mysqladmin -u "$MYSQL_USERt" -p"$MYSQL_PASS" ping --silent
+# }
 
-for i in {1..60}; do
-    if check_mysql; then
-        echo "MYSQL is ready!"
-        break
-    else
-        echo "Waiting for MYSQL to be ready..."
-        sleep 5
-    fi
+# for i in {1..60}; do
+#     if check_mysql; then
+#         echo "MYSQL is ready!"
+#         break
+#     else
+#         echo "Waiting for MYSQL to be ready..."
+#         sleep 5
+#     fi
+# done
+echo "Waiting for MySQL to be ready..."
+
+# Simple MySQL connection check
+until mysqladmin ping -h mysql --silent; do
+    echo "MySQL is unavailable - sleeping"
+    sleep 2
 done
+
+echo "MySQL is up and running!"
